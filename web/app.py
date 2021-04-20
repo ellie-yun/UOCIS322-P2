5ab0516c7eb599e8b04f11037b166cb5a0e2c1d1
Yebin Yun <yebin@Yebins-MacBook-Air.local>
2021-04-19 17:17:01 -0700

    Implemented three functionalities

--- web/app.py
+++ web/app.py
@@ -1,14 +1,27 @@
 """
-John Doe's Flask API.
+Ellie Yun's Flask API.
 """
 
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, abort
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "UOCIS docker demo!\n"
+forbidden_symbols = ["//", "~", ".."]
+
+@app.route('/<path:f_name>')
+def find_file(f_name):
+    check_symbol = [symbol for symbol in forbidden_symbols if symbol in f_name]
+    if len(check_symbol) > 0:
+        abort(403)
+    return send_from_directory("templates", f_name), 200
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(404)
+def not_found_error(e):
+    return render_template('404.html'),404
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')
